664b13727b7665252fb7be0e99adcd44fbb84923
Franco Felicioni <fran.felicioni@gmail.com>
2022-10-31 15:01:37 +0100

    scripts.js created and header functionality added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,33 @@
 //Burger Menu
 
-let burgerMenu = document.querySelector('.nav-bar__burger-menu');
-let navBar = document.querySelector('.nav-bar__container');
+let burgerMenu = document.querySelector(".nav-bar__burger-menu");
+let navBar = document.querySelector(".nav-bar__container");
 
-burgerMenu.addEventListener ('click', () => {
-    navBar.classList.toggle('active')
-    burgerMenu.classList.toggle('animation');
-})
+burgerMenu.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  burgerMenu.classList.toggle("animation");
+});
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
