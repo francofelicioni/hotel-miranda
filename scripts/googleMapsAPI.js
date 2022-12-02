ad3291b924592fdf5c0fc52ecd09c9dc3178580d
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-02 06:56:02 +0100

    Google maps API pins in selected CCAA implemented

--- scripts/googleMapsAPI.js
+++ scripts/googleMapsAPI.js
@@ -2,7 +2,8 @@ import hotels from "../utils/hotelsLocations.json" assert { type: "json" };
 import comunidadesAutonomas from "../utils/ccaa.js";
 import espanaComunidades from "../utils/comEsp.js";
 
-let map, infoWindow, commShapes;
+let map, infoWindow, commShapes, selectedCCAA;
+let pins = [];
 
 function initMap() {
   //Initial position (Main Miranda Hotel, located in Madrid)
@@ -155,13 +156,24 @@ function initMap() {
     select.innerHTML += `<option value='${i}' class='map__option'>${comunidadesAutonomas[i]}</option>`;
   }
 
-  
-
   select.addEventListener("change", (e) => {
-
     if (commShapes) {
       commShapes.setMap(null);
     }
+
+    function getHotels() {
+      if (e.target.value != null && e.target.value != undefined) {
+        return hotels.filter((hotel) => hotel.ca == e.target.value);
+      }
+    }
+
+    let hotelsFiltered = getHotels();
+    // console.log(hotelsFiltered);
+
+    map = new google.maps.Map(document.getElementById("map"), {
+      zoom: hotelsFiltered? 5 : 4,
+      center: hotels[0],
+    });
 
     commShapes = new google.maps.Polygon({
       paths: espanaComunidades[e.target.selectedIndex],
@@ -172,10 +184,29 @@ function initMap() {
       fillOpacity: 0.8,
     });
 
-    // map = new google.maps.Map(document.getElementById("map"), {
-    //   zoom: 6,
-    //   center: initialPosition,
-    // });
+    //Custom Pin
+    const svgMarker = {
+      path: "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+      fillColor: "green",
+      fillOpacity: 1,
+      strokeWeight: 0,
+      rotation: 0,
+      scale: 2,
+      anchor: new google.maps.Point(12, 22),
+    };
+
+    //Add makers to map
+    markers = getHotels().map((h) => {
+      const marker = new google.maps.Marker({
+        position: h,
+        map: map,
+        icon: svgMarker,
+      });
+      return marker;
+    });
+
+    //Make Cluster for close locations
+    new markerClusterer.MarkerClusterer({ map, markers });
     commShapes.setMap(map);
   });
 
