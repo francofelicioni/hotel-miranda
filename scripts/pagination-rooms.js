179637dc3f53faf49d8092cc0c9a460efaa2a2ca
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-05 23:20:48 +0100

    Rooms views modified and styled

--- scripts/pagination-rooms.js
+++ scripts/pagination-rooms.js
@@ -1,9 +1,26 @@
 var swiper = new Swiper(".rooms-page-swiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-  });
+  },
+});
+
+var swiper = new Swiper(".rooms-list-swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+var swiper = new Swiper(".swiper-detail", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
